658e3d8d068eb914b344dbba731f22133279f897
胡文熙 <alice@huwenxideMac-mini-3.local>
2026-07-09 16:32:49 +0800

    Load dotenv fallback path

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.join(__dirname, '.env') });
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
